6c7e7712dd2fffd6b2cf015895f5e8355f2193e4
Sebastian Gnat <upgradesebastian@gmail.com>
2022-09-19 21:36:13 +0200

    🟧 Export Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Properties from './Components/Properties.svelte'
+
   let blur = 0
   let contrast = 100
   let brightness = 100
@@ -151,22 +153,6 @@
     animation: 0.3s scaleUp;
   }
 
-  #properties {
-    position: absolute;
-    top: 0px;
-    right: 0px;
-
-    background-color: rgba(0, 0, 0, 0.2);
-    color: white;
-    z-index: 2;
-    padding: 2rem;
-    margin: 1rem;
-    border-top: 0.5rem solid white;
-    border-radius: 5px;
-
-    animation: 0.3s scaleUp;
-  }
-
   .photo {
     /* transition: 0.1s; */
     z-index: 1;
@@ -291,131 +277,17 @@
   {/if}
 
   {#if UI.showProperties}
-    <div id="properties">
-      <p>
-        Blur
-        <b>{blur}px</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={0}
-          max={5}
-          step={1}
-          bind:value={blur}
-          on:change={timelineSaveState} />
-      </p>
-      <p>
-        Contrast
-        <b>{contrast}%</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={0}
-          max={300}
-          step={10}
-          bind:value={contrast}
-          on:change={timelineSaveState} />
-      </p>
-      <p>
-        Brightness
-        <b>{brightness}%</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={0}
-          max={300}
-          step={10}
-          bind:value={brightness}
-          on:change={timelineSaveState} />
-      </p>
-
-      <p>
-        Saturate
-        <b>{saturate}%</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={0}
-          max={300}
-          step={10}
-          bind:value={saturate}
-          on:change={timelineSaveState} />
-      </p>
-
-      <p>
-        Grayscale
-        <b>{grayscale}%</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={0}
-          max={100}
-          step={5}
-          bind:value={grayscale}
-          on:change={timelineSaveState} />
-      </p>
-
-      <p>
-        Opacity
-        <b>{opacity}%</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={0}
-          max={100}
-          step={5}
-          bind:value={opacity}
-          on:change={timelineSaveState} />
-      </p>
-
-      <p>
-        Sepia
-        <b>{sepia}%</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={0}
-          max={100}
-          step={5}
-          bind:value={sepia}
-          on:change={timelineSaveState} />
-      </p>
-
-      <p>
-        Hue
-        <b>{hue}deg</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={-365}
-          max={365}
-          step={1}
-          bind:value={hue}
-          on:change={timelineSaveState} />
-      </p>
-
-      <p>
-        Invert Colors
-        <b>{invert}%</b>
-      </p>
-      <p>
-        <input
-          type="range"
-          min={0}
-          max={100}
-          step={10}
-          bind:value={invert}
-          on:change={timelineSaveState} />
-      </p>
-    </div>
+    <Properties
+      bind:blur
+      bind:contrast
+      bind:brightness
+      bind:saturate
+      bind:grayscale
+      bind:opacity
+      bind:sepia
+      bind:hue
+      bind:invert
+      {timelineSaveState} />
   {/if}
 
   {#if UI.showTimeline}
---
+++ src/Components/Properties.svelte
@@ -0,0 +1,156 @@
+<script>
+  export let blur,
+    contrast,
+    brightness,
+    saturate,
+    grayscale,
+    opacity,
+    sepia,
+    hue,
+    invert
+  export let timelineSaveState
+</script>
+
+<style>
+  #properties {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+
+    background-color: rgba(0, 0, 0, 0.2);
+    color: white;
+    z-index: 2;
+    padding: 2rem;
+    margin: 1rem;
+    border-top: 0.5rem solid white;
+    border-radius: 5px;
+
+    animation: 0.3s scaleUp;
+  }
+</style>
+
+<div id="properties">
+  <p>
+    Blur
+    <b>{blur}px</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={0}
+      max={5}
+      step={1}
+      bind:value={blur}
+      on:change={timelineSaveState} />
+  </p>
+  <p>
+    Contrast
+    <b>{contrast}%</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={0}
+      max={300}
+      step={10}
+      bind:value={contrast}
+      on:change={timelineSaveState} />
+  </p>
+  <p>
+    Brightness
+    <b>{brightness}%</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={0}
+      max={300}
+      step={10}
+      bind:value={brightness}
+      on:change={timelineSaveState} />
+  </p>
+
+  <p>
+    Saturate
+    <b>{saturate}%</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={0}
+      max={300}
+      step={10}
+      bind:value={saturate}
+      on:change={timelineSaveState} />
+  </p>
+
+  <p>
+    Grayscale
+    <b>{grayscale}%</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={0}
+      max={100}
+      step={5}
+      bind:value={grayscale}
+      on:change={timelineSaveState} />
+  </p>
+
+  <p>
+    Opacity
+    <b>{opacity}%</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={0}
+      max={100}
+      step={5}
+      bind:value={opacity}
+      on:change={timelineSaveState} />
+  </p>
+
+  <p>
+    Sepia
+    <b>{sepia}%</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={0}
+      max={100}
+      step={5}
+      bind:value={sepia}
+      on:change={timelineSaveState} />
+  </p>
+
+  <p>
+    Hue
+    <b>{hue}deg</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={-365}
+      max={365}
+      step={1}
+      bind:value={hue}
+      on:change={timelineSaveState} />
+  </p>
+
+  <p>
+    Invert Colors
+    <b>{invert}%</b>
+  </p>
+  <p>
+    <input
+      type="range"
+      min={0}
+      max={100}
+      step={10}
+      bind:value={invert}
+      on:change={timelineSaveState} />
+  </p>
+</div>
