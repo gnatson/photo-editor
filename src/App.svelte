229cf3af4f724e16fd31d035861e0a4fad107bbc
Sebastian Gnat <upgradesebastian@gmail.com>
2022-09-19 14:12:26 +0200

    add todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   // todo: construct custom filter
+  // animated transitions with svelte/easing... (bounce effect)
+  //  user preferences? (ex. slider step)
 
   const sample_filters = []
   sample_filters.push('filter: blur(5px);')
@@ -8,6 +10,36 @@
   sample_filters.push('filter: hue-rotate(90deg);')
   sample_filters.push('filter: hue-rotate(90deg);')
   sample_filters.push('filter: drop-shadow(16px 16px 20px red) invert(75%);')
+
+  let blur = 0
+  let contrast = 100
+
+  $: style = `filter: blur(${blur}px) contrast(${contrast}%);`
 </script>
 
-<img src="photos/geran-de-klerk-bKhETeDV1WM-unsplash.jpg" alt="" />
+<style>
+  main {
+    user-select: none;
+  }
+</style>
+
+<main>
+  <div id="photo">
+    <img src="photos/geran-de-klerk-bKhETeDV1WM-unsplash.jpg" alt="" {style} />
+    <!-- maybe div.background? -->
+  </div>
+
+  <div id="properties">
+    <p>
+      Blur:
+      <input type="range" min={0} max={5} step={1} bind:value={blur} />
+      <b>{blur}px</b>
+    </p>
+    <p>
+      Contrast:
+      <input type="range" min={0} max={300} step={10} bind:value={contrast} />
+      <b>{contrast}%</b>
+    </p>
+  </div>
+
+</main>
