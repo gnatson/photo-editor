29620d1768bb04e2a9409ac6bde2d1113a0fd7dc
Sebastian Gnat <upgradesebastian@gmail.com>
2022-09-19 14:20:00 +0200

    invert

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,13 @@
   //  user preferences? (ex. slider step)
   // animated preview
 
-  const sample_filters = []
-  sample_filters.push('filter: drop-shadow(16px 16px 20px red) invert(75%);')
-
   let blur = 0
   let contrast = 100
   let grayscale = 0
   let hue = 0
+  let invert = 0
 
-  $: style = `filter: blur(${blur}px) contrast(${contrast}%) grayscale(${grayscale}%) hue-rotate(${hue}deg);`
+  $: style = `filter: blur(${blur}px) contrast(${contrast}%) grayscale(${grayscale}%) hue-rotate(${hue}deg) drop-shadow(16px 16px 20px black) invert(${invert}%);`
 </script>
 
 <style>
@@ -28,16 +26,19 @@
   </div>
 
   <div id="properties">
+
     <p>
       Blur:
       <input type="range" min={0} max={5} step={1} bind:value={blur} />
       <b>{blur}px</b>
     </p>
+    
     <p>
       Contrast:
       <input type="range" min={0} max={300} step={10} bind:value={contrast} />
       <b>{contrast}%</b>
     </p>
+    
     <p>
       Grayscale:
       <input type="range" min={0} max={100} step={5} bind:value={grayscale} />
@@ -48,6 +49,12 @@
       Hue:
       <input type="range" min={-365} max={365} step={1} bind:value={hue} />
       <b>{hue}deg</b>
+    </p>
+
+    <p>
+      Invert Colors:
+      <input type="range" min={0} max={100} step={10} bind:value={invert} />
+      <b>{invert}%</b>
     </p>
   </div>
 
