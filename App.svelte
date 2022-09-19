6ff9bb85bae9109ebdcd3e3421848d2464ee5390
Sebastian Gnat <upgradesebastian@gmail.com>
2022-09-19 03:51:54 +0200

    init

---
+++ App.svelte
@@ -0,0 +1,11 @@
+<script>
+  // todo: construct custom filter
+
+  const sample_filters = []
+  sample_filters.push('filter: blur(5px);')
+  sample_filters.push('filter: contrast(200%);')
+  sample_filters.push('filter: grayscale(80%);')
+  sample_filters.push('filter: hue-rotate(90deg);')
+  sample_filters.push('filter: hue-rotate(90deg);')
+  sample_filters.push('filter: drop-shadow(16px 16px 20px red) invert(75%);')
+</script>
